a7907055d31046d3d4769238eeb716a2db721a58
fhoas <thefhoas@gmail.com>
2024-05-02 19:16:24 +0400

    feat: add functionalities

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Model } from '../model';
+import { TodoItem } from '../todoitems';
 
 @Component({
   selector: 'todo',
@@ -8,18 +9,56 @@ import { Model } from '../model';
 export class TodoComponent {
   displayAll: boolean = true;
 
-  constructor() {}
+  constructor() {
+    this.model.items = this.getItemsFromLS();
+  }
   model = new Model();
 
-  addItems(value: string) {
-    if (value != '') {
-      this.model.items.push({ desc: value, action: false });
+  inputText: string = '';
+
+  addItems() {
+    if (this.inputText != '') {
+      let data = { desc: this.inputText, action: false };
+      this.model.items.push(data);
+      let items = this.getItemsFromLS();
+      items.push(data);
+      this.inputText = '';
     } else {
-      alert("bilgi girin")
+      alert('bilgi girin');
     }
   }
+
+  getItemsFromLS() {
+    let items: TodoItem[] = [];
+
+    let value = localStorage.getItem('items');
+
+    if (value != null) {
+      items = JSON.parse(value);
+    }
+
+    return items;
+  }
+
+  onActionChanged(item: TodoItem) {
+    let items = this.getItemsFromLS();
+
+    localStorage.clear();
+
+    items.forEach((i) => {
+      if (i.desc == item.desc) {
+        i.action = item.action;
+      }
+    });
+    localStorage.setItem('items', JSON.stringify(items));
+  }
+
   getName() {
     return this.model.name;
+  }
+
+  displayCount() {
+    return this.model.items.filter((i) => i.action).length;
   }
 
   getItems() {
